8fd7668617205838ee6776a255e5c0cde070d6b9
Hritam Mondal <hritam.mondal@icloud.com>
2026-03-15 20:41:00 +0530

    Fix ThemeToggle export in theme feature index

--- src/features/theme/index.ts
+++ src/features/theme/index.ts
@@ -1,6 +1,6 @@
 // Public API for the theme feature
 export { ThemeProvider, useTheme } from './state/ThemeContext';
-//export { ThemeToggle } from './components/ThemeToggle';
+export { ThemeToggle } from './components/ThemeToggle';
 export { Settings as ThemeSettings } from './components/Settings';
 export { useToken } from './hooks/useToken';
 export { tokens } from './constants/tokens';
